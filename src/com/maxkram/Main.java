486625ca2330fb90023589a02962411cfc5b1d42
Maksim Kramer <2067717@johnabbottcollege.net>
2021-02-03 18:15:14 -0500

    kak blin eto rabotaet?

--- src/com/maxkram/Main.java
+++ src/com/maxkram/Main.java
@@ -6,18 +6,34 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Enter size of array bigger than 1:");
-        Scanner userInputEntry = new Scanner(System.in);
-        int inputLength =userInputEntry.nextInt();
-        int[] intArray = new int[inputLength];
-        for (int i = 0; i < intArray.length; i++) {
-            intArray[i] = 1 + (int) (Math.random() * 100);
-            System.out.print(intArray[i]);
+        int i, s, j, p;
+        int arr[] = new int[100];
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of an array:");
+        s = sc.nextInt();
+
+        for (i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 100);
+        }
+
+        System.out.print("All prime numbers are:");
+
+        for (i = 0; i < s; i++) {
+            j = 2;
+            p = 1;
+            while (j < arr[i]) {
+                if (arr[i] % j == 0) {
+                    p = 0;
+                    break;
+                }
+                j++;
+            }
+            if (p == 1) {
+                System.out.print(" " + arr[i] + ", ");
+            }
         }
 
     }
-
-
-
 
 }
